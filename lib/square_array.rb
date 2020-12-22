18cfc57df4748570b89a4221f01217dcee3bc6b9
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-22 15:32:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ new_array = []
 
 def square_array(array)
   array.each.length.times do |index|
-  new_array.push(array[index] ** 2)
+  puts new_array.push(array[index] ** 2)
 end
 end
 square_array(new_array)
