07fca5ecbfa7fa6c4d6dd5a6739982467689d9c1
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-22 15:19:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,9 @@
 
-array = [1,2,3]
-def square_array(array)
-  count = 0
-  new_array = []
-  while count < array.length do
-  Math.sqrt("#{array}")
+counter = 0 
+array = [1, 2, 3]
+
+while array[counter] do
+  puts array[counter**2]
+  counter += 1
+  end
 end
-  new_array
-end
-square_array(array)
