8cffc5ca6da7d8103f9dbb2dd5631f6f59c4bb95
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-22 15:28:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 array = [1, 2, 3]
+new_array = []
 
-def square_array(array)
-  array.each ** 2 do |a|
-  puts "#{a}"
+array.length.times do |index|
+new_array.push(array[index] ** 2)
 end
 
-square_array(array)
+new_array
